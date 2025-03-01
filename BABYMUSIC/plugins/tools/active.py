d334757ebf9658c1a9e40fe2194987cb7cccb4f4
lakshhkdusv <cookiesnew0@gmail.com>
2025-03-01 13:16:15 +0530

    Update active.py

--- BABYMUSIC/plugins/tools/active.py
+++ BABYMUSIC/plugins/tools/active.py
@@ -12,7 +12,7 @@ from BABYMUSIC.utils.database import (
 )
 
 
-@app.on_message(filters.command(["activevc", "activevoice","vc"]) & SUDOERS)
+@app.on_message(filters.command(["activevc", "activevoice","all"]) & SUDOERS)
 async def activevc(_, message: Message):
     mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ...")
     served_chats = await get_active_chats()
@@ -44,7 +44,7 @@ async def activevc(_, message: Message):
         )
 
 
-@app.on_message(filters.command(["activev", "activevideo","vvc"]) & SUDOERS)
+@app.on_message(filters.command(["activev", "activevideo","all"]) & SUDOERS)
 async def activevi_(_, message: Message):
     mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ʟɪsᴛ...")
     served_chats = await get_active_video_chats()
